9a4e2c1ec1ea8d87540a5ce2f24f32def1f6ffc1
Dino Garić <dinogaric1@gmail.com>
2022-05-31 16:08:34 +0200

    Layout component finished

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,46 @@
+import styled from '@emotion/styled';
+import { Box } from '@mui/material';
+import { Container } from '@mui/system';
+import React from 'react';
+import { Footer } from '../footer/Footer';
+import Navbar from '../navbar/Navbar';
+
+
+/* The main layout of the page!
+In the page folder we always use this tag (layout)
+So the position is always the same
+*/
+
+const AppBox = styled(Box)({
+	/* This is the top of the page anything under this div is the footer */
+	/* min-height -> makes the footer stay on the bottom of the page */
+
+	minHeight: '100vh',
+	display: 'flex',
+	flexDirection: 'column',
+	justifyContent: 'space-between',
+});
+
+const PageContainer = styled(Container)({
+	background: 'wheat',
+
+	marginBottom: '2em',
+	//marginTop: '2em' <-- Not really needed
+});
+
+const Layout = ({ children }:
+	{ children: any }) => {
+	return (
+		<AppBox>
+			<Box>
+				<Navbar />
+				<PageContainer maxWidth="lg">
+					{children}
+				</PageContainer>
+			</Box>
+			<Footer />
+		</AppBox >
+	)
+}
+
+export default Layout
